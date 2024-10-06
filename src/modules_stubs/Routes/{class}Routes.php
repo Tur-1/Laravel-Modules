c56d8f7765b9d4fbdc057bf940d6150bc1196736
Tur-1 <tur-1i@hotmail.com>
2024-10-06 13:53:19 +0300

    fix routes

--- src/modules_stubs/Routes/{class}Routes.php
+++ src/modules_stubs/Routes/{class}Routes.php
@@ -6,8 +6,8 @@ use {modulePath}\Controllers\{class}Controller;
 Route::middleware(['api'])->prefix('/api')->controller({class}Controller::class)->group(function () {
     Route::get('/{routesName}/get-all', 'getAll');
     Route::get('/{routesName}/get-paginated-list', 'getPaginatedList');
-    Route::post('/{routesName}/store', 'store{Model}');
-    Route::post('/{routesName}/show/{id}', 'show{Model}');
-    Route::post('/{routesName}/update/{id}', 'update{Model}');
-    Route::post('/{routesName}/delete/{id}', 'destroy{Model}');
+    Route::post('/{routesName}/store', 'store');
+    Route::post('/{routesName}/show/{id}', 'show');
+    Route::post('/{routesName}/update/{id}', 'update');
+    Route::post('/{routesName}/delete/{id}', 'destroy');
 });
