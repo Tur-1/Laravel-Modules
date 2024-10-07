f2d8aabf534aeb2ce7968196ed0ba6de99061d67
Tur-1 <tur-1i@hotmail.com>
2024-10-07 09:45:01 +0300

    ee

--- src/Console/Commands/MakeModuleMigration.php
+++ src/Console/Commands/MakeModuleMigration.php
@@ -16,21 +16,25 @@ class MakeModuleMigration extends Command
         $name = $this->argument('name');
         $module = $this->option('module');
 
+        $arguments = [
+            'name' => $name,
+        ];
+
+        // Check if the module option is provided
         if ($module) {
             $modulePath = base_path("app/Modules/{$module}/Database/migrations");
 
+            // Validate if the module path exists
             if (!is_dir($modulePath)) {
                 $this->error("Module '{$module}' does not exist or the migration path is invalid.");
                 return;
             }
-            Artisan::call('make:migration', [
-                'name' => $name,
-                '--path' => "app/Modules/{$module}/Database/migrations",
-            ]);
-        } else {
-            Artisan::call('make:migration', [
-                'name' => $name,
-            ]);
+
+            // Add the path to the arguments if the module is specified
+            $arguments['--path'] = "app/Modules/{$module}/Database/migrations";
         }
+
+        // Run the Artisan command with or without the path
+        Artisan::call('make:migration', $arguments);
     }
 }
